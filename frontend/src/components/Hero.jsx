8fd9506db623ea9e9dc0b571bddfad37349993c1
SufailSLX <sufailxd@gmail.com>
2025-08-06 16:03:26 +0530

    Bug fixes 1.1

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
     <section className='max-padd-container pt-[99px]'>
         <div className='max-padd-container bg-hero bg-center bg-cover bg-no-repeat h-[655px] w-full rounded-3xl'>
             <div className='relative top-32 xs:top-52'>
-                <span className='medium-18'>Welcome to name...!</span>
-                <h1 className='h1 capitalize max-w-[40rem]'>Discover Exceptional Homes with name...</h1>
+                <span className='medium-18'>Welcome to LandX...!</span>
+                <h1 className='h1 capitalize max-w-[40rem]'>Discover Exceptional Homes with LandX...</h1>
                 <p className='my-10 max-w-[33rem]'>In the heart of a bustling city, a small café nestled between towering skyscrapers offers a serene escape for its visitors.
                 The aroma of freshly brewed coffee mingles with the scent of baked pastries, drawing in passersby. Inside, sunlight streams through large windows,
                 illuminating wooden tables where people gather to share stories and ideas. Artists sketch in the corners, while friends catch up over steaming cups.</p>
